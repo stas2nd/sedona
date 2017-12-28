6d5115e99c7ac58685f5727627bf0ce0c929a09e
Анастасия <holy_mel@mail.ru>
2017-12-28 15:27:26 +0300

    js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-﻿
+﻿var link = document.querySelector(".searching-hotels__link");
+var formwrapper = document.querySelector(".form-wrapper");
+
+if (link && formwrapper) {
+  link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    formwrapper.classList.toggle("display_none");
+  });
+}
